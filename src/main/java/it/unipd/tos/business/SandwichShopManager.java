676e43b32d117dc5556243fd4755ff972c81fb7f
dlazzaro <dmlazzaro98@gmail.com>
2019-12-08 16:01:43 +0100

    aggiunte funzionalità di sconto per più di 5 panini e per spesa maggiore di 50 euro

--- src/main/java/it/unipd/tos/business/SandwichShopManager.java
+++ src/main/java/it/unipd/tos/business/SandwichShopManager.java
@@ -1,6 +1,10 @@
+////////////////////////////////////////////////////////////////////
+// Davide Maria Lazzaro 1162190
+////////////////////////////////////////////////////////////////////
 package it.unipd.tos.business;
 
 import it.unipd.tos.model.MenuItem;
+import it.unipd.tos.model.MenuItem.ItemType;
 
 import java.util.List;
 
@@ -16,7 +20,26 @@ public class SandwichShopManager implements TakeAwayBill {
         }
 		
 		double totale = itemsOrdered.stream().mapToDouble(x->x.getPrezzo()).sum();
+		
+		
+			if(itemsOrdered.stream().filter(s -> s.getTipo() == ItemType.Panino).count() > 5)
+			{
+				totale-= (itemsOrdered.stream().filter(s -> s.getTipo() == ItemType.Panino).mapToDouble(d -> d.getPrezzo()).min().orElse(0.00)) / 2.0;
+			}
+			
+			if(itemsOrdered.stream().mapToDouble(x->x.getPrezzo()).sum()>50.0)
+			{
+				totale-=totale*0.1;
+			}
+			
+			if(itemsOrdered.stream().count() > 30) {
+	            System.err.println("L'ordine non può superare i 30 elementi");
+	            throw new TakeAwayBillException();
+	        }
+		
 		return totale;
+		
+		
 
 	}
 	
--- src/test/java/it/unipd/tos/SandwichShopManagerTest.java
+++ src/test/java/it/unipd/tos/SandwichShopManagerTest.java
@@ -1,34 +1,37 @@
+////////////////////////////////////////////////////////////////////
+// Davide Maria Lazzaro 1162190
+////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import org.junit.Test;
-
 import it.unipd.tos.business.SandwichShopManager;
+import it.unipd.tos.business.exception.TakeAwayBillException;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.MenuItem.ItemType;
 
-import java.util.List;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 
 
-public class SandwichShopManagerTest {
+
+public class SandwichShopManagerTest extends Assert{
 
 	
 	private SandwichShopManager prova = new SandwichShopManager();
     private List<MenuItem> listaProva = new ArrayList<MenuItem>();
 
     MenuItem primavera= new MenuItem(ItemType.Panino, "Panino Primavera", 5.0f);
-    MenuItem doppiaCarne= new MenuItem(ItemType.Panino, "Panino Vegetariano", 6.0f);
-    MenuItem vegetariano =new MenuItem(ItemType.Panino, "Panino Doppia Carne", 7.0f);
+    MenuItem vegetariano= new MenuItem(ItemType.Panino, "Panino Vegetariano", 6.0f);
+    MenuItem doppiaCarne =new MenuItem(ItemType.Panino, "Panino Doppia Carne", 7.0f);
     MenuItem semplice =new MenuItem(ItemType.Panino, "Panino Semplice", 3.0f);
     MenuItem cocaCola=new MenuItem(ItemType.Bevanda, "coca cola ", 1.0f);
-    MenuItem olive=new MenuItem(ItemType.Fritto, "Olive Ascolane", 3.0f);
-    MenuItem mozzarelline=new MenuItem(ItemType.Fritto, "Mozzarelline", 3.0f);
+    MenuItem olive=new MenuItem(ItemType.Fritto, "Olive Ascolane", 3.5f);
+    MenuItem mozzarelline=new MenuItem(ItemType.Fritto, "Mozzarelline", 3.5f);
     
     @Before
     public void emptyList() {
@@ -36,7 +39,7 @@ public class SandwichShopManagerTest {
     }
     
 	@Test
-	public void getOrderTotalPriceTest() {
+	public void getOrderTotalPriceTest() throws TakeAwayBillException{
 		listaProva.add(primavera);
 		listaProva.add(doppiaCarne);
 		listaProva.add(vegetariano);
@@ -44,7 +47,37 @@ public class SandwichShopManagerTest {
 		listaProva.add(cocaCola);
 		listaProva.add(olive);
 		listaProva.add(mozzarelline);
+        
+		assertEquals(prova.getOrderPrice(listaProva), 29.0, 0.0);
+
 
 	}
-
+	
+	@Test
+	public void getOrderTotalPriceDiscountWith5Sandwich() throws TakeAwayBillException {
+		listaProva.add(primavera);
+		listaProva.add(doppiaCarne);
+		listaProva.add(primavera);
+		listaProva.add(semplice);
+		listaProva.add(vegetariano);
+		listaProva.add(semplice);
+		listaProva.add(cocaCola);
+		
+		assertEquals(prova.getOrderPrice(listaProva), 28.5, 0.0);
+	}
+	@Test
+	public void getOrderTotalPriceAfter10PercentDiscountIfTotalGreaterThan50() throws TakeAwayBillException{
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		listaProva.add(doppiaCarne);
+		
+		assertEquals(prova.getOrderPrice(listaProva), 53.55, 0.0);
+	}
+	
 }
